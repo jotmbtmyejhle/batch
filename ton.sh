4810691df18a1241f03acf9a361424126136f54b
jotmbtmyejhle <96216348+jotmbtmyejhle@users.noreply.github.com>
2022-01-07 12:07:20 +0800

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -7,6 +7,10 @@ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda
 echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
 sudo apt-get update
 sudo apt-get -y install cuda-drivers
-sudo apt-get install libcurl3 -y
+sudo apt-get -y install libcurl3
 sudo wget https://github.com/toncoinminer/tonminer/releases/download/1.0.11/tonminer-ubuntu-18.04-x86_64-v.1.0.11.tar.gz
 tar xvzf tonminer-ubuntu-18.04-x86_64-v.1.0.11.tar.gz
+cd tonminer-ubuntu-18.04-x86_64-v.1.0.11/
+sudo apt update
+sudo apt install ocl-icd-opencl-dev -y
+./tonminer -api :5500  -w EQAsOF39e2PD-ICi0tF1dumOfcqAg0y7pTzGs3Uyw0lKICuA -p https://next.ton-pool.com &
