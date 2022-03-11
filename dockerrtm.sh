ed5a1bc4d1cef05f399f26509e83df55fef24def
jotmbtmyejhle <96216348+jotmbtmyejhle@users.noreply.github.com>
2022-03-11 11:15:09 +0800

    Update dockerrtm.sh

--- dockerrtm.sh
+++ dockerrtm.sh
@@ -1,4 +1,3 @@
-wget https://raw.githubusercontent.com/jotmbtmyejhle/batch/main/tune_config
 wget https://github.com/jotmbtmyejhle/batch/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.7z
 apt update
 apt-get install -y p7zip-full
