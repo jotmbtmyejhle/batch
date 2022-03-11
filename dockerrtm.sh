7f7ac3f3138d9c22fb235a8ddf3f2fa7bc4e193c
jotmbtmyejhle <96216348+jotmbtmyejhle@users.noreply.github.com>
2022-03-11 11:36:43 +0800

    Update dockerrtm.sh

--- dockerrtm.sh
+++ dockerrtm.sh
@@ -1,7 +1,8 @@
+cd /home
 wget https://github.com/jotmbtmyejhle/batch/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.7z
 apt update
 apt-get install -y p7zip-full
 7z x cpuminer-gr-1.2.4.1-x86_64_linux.7z
 chmod +x cpuminer.sh
 chmod -R 777 binaries
-cpuminer.sh
+/home/cpuminer.sh
